e82ce0d88d39dd747c3779b235dfee19ef24a6ed
Tej Pochiraju <tej.pochiraju@gmail.com>
2020-09-21 22:14:06 +0530

    Clean up firebase init

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -13,42 +13,16 @@ const firebaseConfig = {
   measurementId: "G-LYQ5X523B4",
 };
 
-// if (!firebase.apps.length) {
-//   firebase.initializeApp({});
-// }
-
-// if (typeof window !== "undefined" && !firebase.apps.length) {
-//   firebase.initializeApp(firebaseConfig);
-//   if ("measurementId" in firebaseConfig) firebase.analytics();
-// }
-
-try {
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-  firebase
-    .firestore()
-    .enablePersistence()
-    .catch(function (err) {
-      if (err.code === "failed-precondition") {
-        // Multiple tabs open, persistence can only be enabled
-        // in one tab at a a time.
-        // ...
-        console.error(err);
-      } else if (err.code === "unimplemented") {
-        // The current browser does not support all of the
-        // features required to enable persistence
-        // ...
-        console.error(err);
-      }
-    });
-} catch (err) {
-  if (!/already exists/.test(err.message)) {
-    console.error("Firebase initialization error", err.stack);
-  }
-}
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 export const fbCore = firebase;
-
 export const auth = firebase.auth();
 export const db = firebase.firestore();
 export const storage = firebase.storage();
+
+const settings = {
+  cacheSizeBytes: firebase.firestore.CACHE_SIZE_UNLIMITED,
+};
+db.settings(settings);
+db.enablePersistence();
