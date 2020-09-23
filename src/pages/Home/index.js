1ecfb219cc6492735310f1fd04178db664e8ccc8
Tej Pochiraju <tej.pochiraju@gmail.com>
2020-09-23 17:56:56 +0530

    Clean up home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,9 +2,7 @@ import React from "react";
 import Dropzone from "../../components/Dropzone";
 import UserLibrary from "../../components/UserLibrary";
 import { makeStyles } from "@material-ui/core/styles";
-// import Modal from "@material-ui/core/Modal";
 import { navigate } from "@reach/router";
-// import ContentDetails from "../../components/ContentDetails";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,46 +18,15 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-// function getModalStyle() {
-//   const top = 500;
-//   const left = 50;
-
-//   return {
-//     top: `${top}%`,
-//     left: `${left}%`,
-//     transform: `translate(-${top}%, -${left}%)`,
-//   };
-// }
-
 function Home() {
   const classes = useStyles();
-  // const [modalStyle] = React.useState(getModalStyle);
-  // const [open, setOpen] = React.useState(false);
-  // const [contentId, setContentId] = React.useState();
-  // const handleOpen = () => {
-  //   setOpen(true);
-  // };
-
-  // const handleClose = () => {
-  //   setOpen(false);
-  // };
   const openContent = (id) => {
-    // setContentId(id);
-    // handleOpen();
     navigate(`/content/${id}`);
   };
   return (
     <div className={classes.root}>
       <Dropzone />
       <UserLibrary openContent={openContent} />
-      {/* <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        {contentId && <ContentDetails id={contentId} />}
-      </Modal> */}
     </div>
   );
 }
